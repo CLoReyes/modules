03a8ff1b35cc924fc69de7aaeba9d07a3e544aaf
carlos.reyes <carlos.reyes@MU-C1-18.mullen.pvt.k12.co.us>
2018-08-31 08:53:19 -0600

    modules

---
+++ modules/src/modules/Modules2.java
@@ -0,0 +1,28 @@
+package modules;
+
+import java.util.Scanner;
+
+public class Modules2
+	{
+
+		public static void main(String[] args)
+			{
+				leapYearCalc();
+			}
+
+		private static void leapYearCalc()
+			{
+				Scanner sc = new Scanner (System.in);
+				
+				if (num % 4 && num % 100 && num % 400)
+					{
+						System.out.println("Leap Year");
+					}
+				else
+					{
+						
+					}
+			
+			}
+
+	}
---
+++ modules/src/modules/modulesss.java
@@ -0,0 +1,43 @@
+package modules;
+
+import java.util.Scanner;
+
+public class modulesss
+	{
+		public static void main(String[] args)
+			{
+			
+				leapYearCalc();
+			}
+
+		private static void leapYearCalc()
+			{
+		
+				while (true)
+					{
+						
+						System.out.println("Pick a year and we'll see if it is a leap year");
+						Scanner userInput = new Scanner (System.in);
+						int userYear = userInput.nextInt();
+						if (userYear % 100 == 0)
+							
+							{
+								if (userYear % 400 == 0)
+								
+									{
+										System.out.println("Leap YEar");
+									}
+								
+								else
+									{
+										System.out.println("Not a Leap Year");
+									}
+					
+					
+							}
+				
+	
+					}
+			}
+		
+	}
